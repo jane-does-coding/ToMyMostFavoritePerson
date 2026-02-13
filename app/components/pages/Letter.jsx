b4099d81958cace92eb0a2fdf33d28c4fd5a24eb
Jane <yevheniiasimaka@gmail.com>
2026-02-12 23:05:33 -0700

    position fix

--- app/components/pages/Letter.jsx
+++ app/components/pages/Letter.jsx
@@ -8,12 +8,6 @@ const Letter = () => {
 			<h1 className="absolute top-[6vh] left-[50%] translate-x-[-50%] sugar text-[10vh] font-extralight mx-auto text-center w-fit">
 				You got a letter...
 			</h1>
-			<p className="absolute bottom-[32vh] z-99 sugar text-[3vh] font-extralight left-[50%] translate-x-[-50%] w-[50vw] bg-blue-300/0 leading-[5.5vh]">
-				I was really looking forward to spending this Valentine's Day with you,
-				but it can't happen. So I made a website for you, to show you how
-				valuable you've been to me, in my life. This is all of my feelings and
-				emotions, put together. Which I've really wanted to share with you.
-			</p>
 
 			<a
 				href="/page1"
@@ -22,10 +16,18 @@ const Letter = () => {
 				<Button text={"Continue"} />
 			</a>
 
-			<img
-				src="/imgs/envelope-hearts-nobg.png"
-				className="absolute bottom-0 z-0 left-[50%] translate-x-[-50%] w-[70vw] h-[80vh] drop-shadow-2xl"
-			/>
+			<div className="absolute bottom-0 z-0 left-[50%] translate-x-[-50%] w-[70vw] h-[80vh]">
+				<p className="absolute top-[25vh] z-99 sugar text-[2.8vh] font-extralight left-[50%] translate-x-[-50%] w-[50vw] bg-blue-300/0 leading-[5vh]">
+					I was really looking forward to spending this Valentine's Day with
+					you, but it can't happen. So I made a website for you, to show you how
+					valuable you've been to me, in my life. This is all of my feelings and
+					emotions, put together. Which I've really wanted to share with you.
+				</p>
+				<img
+					src="/imgs/envelope-hearts-nobg.png"
+					className="  w-[70vw] h-[80vh] drop-shadow-2xl"
+				/>
+			</div>
 		</div>
 	);
 };
